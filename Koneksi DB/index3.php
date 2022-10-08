0c07459e81dd9d6e07c5e492b50b03688fac1bc7
Ulwan Luthfi <luthfi030706@gmail.com>
2022-10-08 15:23:38 +0700

    Koneksi DB

---
+++ Koneksi DB/index3.php
@@ -0,0 +1,35 @@
+<?php
+require "function.php";
+$siswa = query("SELECT * FROM siswa")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Koneksi DB</title>
+</head>
+
+<body>
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>Nama</th>
+      <th>Aksi</th>
+    </tr>
+    <?php $i = 1; foreach ($siswa as $s) : ?>
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><img src="img/<?= $s['gambar']; ?>" width="60"></td>
+        <td><?= $s['nama']; ?></td>
+        <td><a href="detail.php?id=<?= $s['id']; ?>">Lihat Detail</a></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+
+</html>
